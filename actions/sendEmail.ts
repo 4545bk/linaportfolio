743c24284541726f718d081dab6d319f29eaedbe
Natnael Gizaw <116101430+4545bk@users.noreply.github.com>
2024-04-05 20:33:51 +0300

    Update sendEmail.ts

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -26,8 +26,8 @@ export const sendEmail = async (formData: FormData) => {
   let data;
   try {
     data = await resend.emails.send({
-      from: "Contact Form <bedemariyamgizaw@gmail.com>",
-      to: "bedemariyamgizaw@gmail.com",
+      from: 'onboarding@resend.dev',
+      to: 'natnaelgizawayano@gmail.com',
       subject: "Message from contact form",
       reply_to: senderEmail,
       react: React.createElement(ContactFormEmail, {
