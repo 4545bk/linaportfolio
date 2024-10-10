58af7f433b87ac1779d89812bd8749f8444c6cdb
4545bk <natnaelgizawayano@gmail.com>
2024-10-11 01:00:32 +0300

    the rotation parts

--- components/intro.tsx
+++ components/intro.tsx
@@ -78,7 +78,7 @@ export default function Intro() {
       >
         <Link
           href="https://t.me/betrisho"
-          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+          className="group bg-gray-900 text-white mb-1 px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
           onClick={() => {
             setActiveSection("Contact");
             setTimeOfLastClick(Date.now());
--- components/projects.tsx
+++ components/projects.tsx
@@ -5,13 +5,46 @@ import SectionHeading from "./section-heading";
 import { projectsData } from "@/lib/data";
 import Project from "./project";
 import { useSectionInView } from "@/lib/hooks";
+import styles from './Styles/Projects.module.css';
 
+ 
 export default function Projects() {
   const { ref } = useSectionInView("Girls", 0.5);
 
   return (
     <section ref={ref} id="projects" className="scroll-mt-28 mb-2">
-      <SectionHeading> Choose girls</SectionHeading>
+      {/* Rotate phone message with embedded Giphy GIF */}
+      <div className="flex flex-col items-center justify-center mb-4">
+        <span className="text-lg font-semibold text-center mt-2 text-white">
+          Rotate Your phone for better Experience  
+        </span>
+
+        <div className="relative flex items-center justify-center">
+          <iframe
+            src="https://giphy.com/embed/ORjDoknEwfqpHPUHRt"
+            width="100"
+            height="100"
+            style={{
+              border: 'none',
+              filter: 'grayscale(50%) brightness(0.8) contrast(120%)',
+            }}
+            frameBorder="0"
+            className="giphy-embed relative z-10"
+            allowFullScreen
+          ></iframe>
+
+          <div className={`${styles.animateShine} absolute inset-0 border-4 border-transparent rounded-full`}></div>
+        </div>
+
+        <span className="text-lg font-semibold text-center mt-2 text-white">
+          ፎቶ ለማየት ስልኮትን አግድም ይያዙ
+        </span>
+      </div>
+
+      <div className="mt-8">
+        <SectionHeading>Choose girls</SectionHeading>
+      </div>
+
       <div>
         {projectsData.map((project, index) => (
           <React.Fragment key={index}>
